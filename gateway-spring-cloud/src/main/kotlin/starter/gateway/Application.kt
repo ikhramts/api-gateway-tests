e3bed55ba1c6248e79564da909ff26076749466f
Iouri Khramtsov <ikhramtsov@hoopp.com>
2019-01-22 15:38:11 -0500

    Added OAuth2 libs

--- gateway-spring-cloud/src/main/kotlin/starter/gateway/Application.kt
+++ gateway-spring-cloud/src/main/kotlin/starter/gateway/Application.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
+
 class Application
 
 fun main(args: Array<String>) {
